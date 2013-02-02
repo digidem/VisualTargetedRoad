71c958b2ea704f5d79691e14b76c7cf57c070f27
Juan C. Müller <jcmuller@gmail.com>
2013-02-02 15:53:46 -0500

    Load some sample data

---
+++ public/javascripts/test_data.js
@@ -0,0 +1,29 @@
+var categories = [
+  { id: 1, name: "Hospital" },
+  { id: 2, name: "Police Station" },
+  { id: 3, name: "Psycho-social support" },
+  { id: 4, name: "Church" }
+];
+var data = [
+  {
+    lat: 18.540286,
+    lon: -72.320037,
+    title: "Hospital 1",
+    description: "",
+    category: "Hospital"
+  },
+  {
+    lat: 18.140286,
+    lon: -72.920037,
+    title: "Hospital 2",
+    description: "",
+    category: "Hospital"
+  },
+  {
+    lat: "18.5292345",
+    lon: "-72.3339691",
+    title: "Hospital Prince",
+    description: "",
+    category: "Hospital"
+  }
+]
