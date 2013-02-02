0da189283c85907b2a902a8f84bcf6cabe54a167
Benjamin Corey Wilkins <becowilk@gmail.com>
2013-02-02 15:49:40 -0500

    make centerAt chainable

--- public/javascripts/map_renderer.js
+++ public/javascripts/map_renderer.js
@@ -1,10 +1,19 @@
 (function(window) {
   function MapRenderer(_domElement) {
     this.domElement = _domElement;
-	
-	this.centerAt = function(location) {
-		var map = L.map(this.domElement).setView([location.lat, location.lon], 13);
-	}
+
+    this.centerAt = function(location) {
+      console.log(this);
+      var map = L.map(this.domElement);
+      map.setView([location.lat, location.lon], 13);
+
+      var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+                  { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' }
+                 );
+       tiles.addTo(map);
+      return this;
+    };
+    return this;
   }
   window.MapRenderer = MapRenderer;
 })(window);
