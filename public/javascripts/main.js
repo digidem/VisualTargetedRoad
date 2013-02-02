55dbf17021ad28d3e528dc3d4d9bfe680de2efcf
Benjamin Corey Wilkins <becowilk@gmail.com>
2013-02-02 16:02:28 -0500

    js work

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,3 @@
 console.log('ready!');
+window.map = new MapRenderer('map').centerAt({lat: "1.2345", lon: "-1.3245"});
+console.log(map);
--- public/javascripts/map_renderer.js
+++ public/javascripts/map_renderer.js
@@ -1,9 +1,10 @@
 (function(window) {
+  
   function MapRenderer(_domElement) {
     this.domElement = _domElement;
 
     this.centerAt = function(location) {
-      console.log(this);
+
       var map = L.map(this.domElement);
       map.setView([location.lat, location.lon], 13);
 
@@ -11,9 +12,12 @@
                   { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' }
                  );
        tiles.addTo(map);
+
       return this;
     };
+
     return this;
+
   }
   window.MapRenderer = MapRenderer;
 })(window);
