f7742bfc219156b0a03535175736aba5431e9855
Juan C. Müller <jcmuller@gmail.com>
2013-02-02 16:02:15 -0500

    Cleanup

--- public/javascripts/map_renderer.js
+++ public/javascripts/map_renderer.js
@@ -1,10 +1,10 @@
 (function(window) {
   function MapRenderer(_domElement) {
     this.domElement = _domElement;
-	
-	this.centerAt = function(location) {
-		var map = L.map(this.domElement).setView([location.lat, location.lon], 13);
-	}
+
+    this.centerAt = function(location) {
+      var map = L.map(this.domElement).setView([location.lat, location.lon], 13);
+    }
   }
   window.MapRenderer = MapRenderer;
 })(window);
